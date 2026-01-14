a8251ce9100c0e1123e0e803956ec5b911791936
cheongimun <cheongimun.ai@gmail.com>
2026-01-14 11:48:59 +0900

    Update realtime-data.js - 8AM KST, data comparison feature

--- js/realtime-data.js
+++ js/realtime-data.js
@@ -66,91 +66,134 @@
       // Comprehensive text replacement patterns
       // [old pattern, new value]
       const replacements = [
-        // MAU patterns
+        // MAU patterns (all historical values)
+        ['70,023명', formatNum(data.mau) + '명'],
+        ['70,023', formatNum(data.mau)],
+        ['66,099명', formatNum(data.mau) + '명'],
+        ['66,099', formatNum(data.mau)],
+        ['57,786명', formatNum(data.mau) + '명'],
+        ['57,786', formatNum(data.mau)],
+        ['7.0만명', formatMan(data.mau) + '명'],
+        ['7.0만', formatMan(data.mau)],
         ['6.6만명', formatMan(data.mau) + '명'],
         ['6.6만', formatMan(data.mau)],
         ['6.0만', formatMan(data.mau)],
         ['60,000', formatNum(data.mau)],
-        ['66,099명', formatNum(data.mau) + '명'],
-        ['66,099', formatNum(data.mau)],
-        ['57,786', formatNum(data.mau)],
 
-        // MRR/Revenue patterns
-        ['8,663만원', formatManWonUnit(data.revenue)],
-        ['8,663만', formatManWon(data.revenue)],
+        // MRR/Revenue patterns (all historical values)
+        ['106,489,300원', formatNum(data.revenue) + '원'],
+        ['106,489,300', formatNum(data.revenue)],
+        ['108,094,700원', formatNum(data.revenue) + '원'],
+        ['108,094,700', formatNum(data.revenue)],
+        ['87,922,500원', formatNum(data.revenue) + '원'],
+        ['87,922,500', formatNum(data.revenue)],
+        ['86,631,200원', formatNum(data.revenue) + '원'],
+        ['86,631,200', formatNum(data.revenue)],
+        ['10,649만원', formatManWonUnit(data.revenue)],
+        ['10,649만', formatManWon(data.revenue)],
+        ['10,809만원', formatManWonUnit(data.revenue)],
+        ['10,809만', formatManWon(data.revenue)],
         ['8,792만원', formatManWonUnit(data.revenue)],
         ['8,792만', formatManWon(data.revenue)],
-        ['87,922,500', formatNum(data.revenue)],
-        ['108,094,700', formatNum(data.revenue)],
+        ['8,663만원', formatManWonUnit(data.revenue)],
+        ['8,663만', formatManWon(data.revenue)],
+        ['1.08억원', formatEok(data.revenue) + '원'],
+        ['1.08억', formatEok(data.revenue)],
+        ['1.06억', formatEok(data.revenue)],
         ['0.88억', formatEok(data.revenue)],
+        ['0.87억', formatEok(data.revenue)],
+        ['MRR 1.08억', 'MRR ' + formatEok(data.revenue)],
         ['MRR 0.88억', 'MRR ' + formatEok(data.revenue)],
 
         // ARR patterns
+        ['12.8억원', formatEokShort(data.arr) + '원'],
+        ['12.8억', formatEokShort(data.arr)],
+        ['13.0억원', formatEokShort(data.arr) + '원'],
+        ['13.0억', formatEokShort(data.arr)],
+        ['10.6억원', formatEokShort(data.arr) + '원'],
+        ['10.6억', formatEokShort(data.arr)],
+        ['10.5억', formatEokShort(data.arr)],
         ['10.4억원', formatEokShort(data.arr) + '원'],
         ['10.4억', formatEokShort(data.arr)],
-        ['10.5억', formatEokShort(data.arr)],
-        ['10.6억', formatEokShort(data.arr)],
-        ['13.0억', formatEokShort(data.arr)],
 
         // ARPPU patterns
+        ['35,928원', formatWon(data.arppu)],
+        ['35,928', formatNum(data.arppu)],
+        ['35,924원', formatWon(data.arppu)],
+        ['35,924', formatNum(data.arppu)],
+        ['35,843원', formatWon(data.arppu)],
+        ['35,843', formatNum(data.arppu)],
         ['34,820원+', formatWon(data.arppu) + '+'],
         ['34,820원', formatWon(data.arppu)],
         ['34,820', formatNum(data.arppu)],
-        ['35,843원', formatWon(data.arppu)],
-        ['35,843', formatNum(data.arppu)],
-        ['35,924원', formatWon(data.arppu)],
-        ['35,924', formatNum(data.arppu)],
 
         // Paying users
-        ['2,453명', formatNum(data.payingUsers) + '명'],
-        ['2,453', formatNum(data.payingUsers)],
+        ['2,964명', formatNum(data.payingUsers) + '명'],
+        ['2,964', formatNum(data.payingUsers)],
         ['3,009명', formatNum(data.payingUsers) + '명'],
         ['3,009', formatNum(data.payingUsers)],
+        ['2,453명', formatNum(data.payingUsers) + '명'],
+        ['2,453', formatNum(data.payingUsers)],
 
         // CAC patterns
-        ['923원', formatWon(data.cac)],
-        ['923', formatNum(data.cac)],
+        ['22,967원', formatWon(data.cac)],
+        ['22,967', formatNum(data.cac)],
         ['21,590원', formatWon(data.cac)],
         ['21,590', formatNum(data.cac)],
-        ['20,580원', formatWon(data.cac)],
         ['20,903원', formatWon(data.cac)],
+        ['20,903', formatNum(data.cac)],
+        ['20,580원', formatWon(data.cac)],
+        ['923원', formatWon(data.cac)],
+        ['923', formatNum(data.cac)],
 
         // LTV/CAC patterns
         ['37.7x', formatX(data.ltvCac)],
-        ['1.66x', formatX(data.ltvCac)],
         ['1.74x', formatX(data.ltvCac)],
         ['1.72x', formatX(data.ltvCac)],
+        ['1.66x', formatX(data.ltvCac)],
+        ['1.56x', formatX(data.ltvCac)],
 
         // ROAS patterns
+        ['1.56x', formatX(data.roas)],
         ['1.42x', formatX(data.roas)],
+        ['ROAS 1.56x', 'ROAS ' + formatX(data.roas)],
         ['ROAS 1.42x', 'ROAS ' + formatX(data.roas)],
 
         // Conversion rate patterns
-        ['3.76%', formatPercent(data.conversionRate)],
-        ['4.11%', formatPercent(data.conversionRate)],
-        ['4.24%', formatPercent(data.conversionRate)],
         ['4.55%', formatPercent(data.conversionRate)],
+        ['4.24%', formatPercent(data.conversionRate)],
+        ['4.23%', formatPercent(data.conversionRate)],
+        ['4.11%', formatPercent(data.conversionRate)],
+        ['3.76%', formatPercent(data.conversionRate)],
 
         // D1 Retention patterns
-        ['4.4%', formatPercent(data.d1Retention)],
+        ['4.68%', formatPercent(data.d1Retention)],
         ['4.45%', formatPercent(data.d1Retention)],
+        ['4.4%', formatPercent(data.d1Retention)],
         ['4.39%', formatPercent(data.d1Retention)],
 
         // Stickiness patterns
         ['4.1%', formatPercent(data.stickiness)],
+        ['3.85%', formatPercent(data.stickiness)],
+        ['3.83%', formatPercent(data.stickiness)],
         ['3.79%', formatPercent(data.stickiness)],
         ['3.66%', formatPercent(data.stickiness)],
-        ['3.83%', formatPercent(data.stickiness)],
 
         // Gross Margin patterns
+        ['84.9%', formatPercentShort(data.grossMargin) + '%'],
+        ['85.7%', formatPercentShort(data.grossMargin) + '%'],
         ['75.7%', formatPercentShort(data.grossMargin) + '%'],
         ['75.3%', formatPercentShort(data.grossMargin) + '%'],
-        ['85.7%', formatPercentShort(data.grossMargin) + '%'],
 
         // Repurchase rate patterns
-        ['2.73%', formatPercent(data.repurchaseRate)],
+        ['2.94%', formatPercent(data.repurchaseRate)],
         ['2.92%', formatPercent(data.repurchaseRate)],
-        ['2.8%', formatPercent(data.repurchaseRate)]
+        ['2.8%', formatPercent(data.repurchaseRate)],
+        ['2.73%', formatPercent(data.repurchaseRate)],
+
+        // Net margin (순마진) patterns - calculated from revenue
+        ['65,631,200원', formatNum(Math.round(data.revenue * data.grossMargin / 100)) + '원'],
+        ['65,631,200', formatNum(Math.round(data.revenue * data.grossMargin / 100))]
       ];
 
       // Perform text replacements in the DOM
